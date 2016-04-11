6ea2ba7f5b1989f8f0fef11d27c3be0ab0954d7d
SlickJava <slickjava@gmail.com>
2016-04-11 21:30:25 +1200

    Test commit #2. Added a method.

--- SmokeyHotels/src/com/smokeyhotel/Main.java
+++ SmokeyHotels/src/com/smokeyhotel/Main.java
@@ -5,6 +5,13 @@ public class Main {
 	public static void main(String args[])
 	{
 		System.out.println("Test");
+		Main main = new Main();
+		main.testMethod();
+	}
+	
+	public void testMethod()
+	{
+		System.out.println("testMethod();");
 	}
 
 }
