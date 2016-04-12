60237b75cb86561dd0c6a60dfa345c35ca019294
SlickJava <slickjava@gmail.com>
2016-04-12 22:34:55 +1200

    Few more changes to Guest class.

--- SmokeyHotels/src/com/smokeyhotel/people/guest/Guest.java
+++ SmokeyHotels/src/com/smokeyhotel/people/guest/Guest.java
@@ -1,12 +1,27 @@
 package com.smokeyhotel.people.guest;
 
+import java.sql.Date;
+
 import com.smokeyhotel.people.Person;
 
 public class Guest extends Person{
-
+	
+	private String creditCardNumber;
+	
 	public Guest(int age, String name, String address, String suburb,
-			String phone) {
+			String phone, String creditCardNumber) {
 		super(age, name, address, suburb, phone);
+		this.creditCardNumber = creditCardNumber;
+	}
+	
+	public String getCreditCardNumber()
+	{
+		return this.creditCardNumber;
+	}
+	
+	public void setCreditCardNumber(String number)
+	{
+		this.creditCardNumber = number;
 	}
 	
 
