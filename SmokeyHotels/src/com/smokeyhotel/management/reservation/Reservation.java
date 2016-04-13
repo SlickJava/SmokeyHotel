0d3cdef8ff591c23b64671997f1d685a2ff26a1c
SlickJava <slickjava@gmail.com>
2016-04-13 22:29:55 +1200

    Moving methods to appropriate positions.

--- SmokeyHotels/src/com/smokeyhotel/management/reservation/Reservation.java
+++ SmokeyHotels/src/com/smokeyhotel/management/reservation/Reservation.java
@@ -1,28 +1,29 @@
 package com.smokeyhotel.management.reservation;
 
 import com.smokeyhotel.people.Person;
+import com.smokeyhotel.room.Room;
 
 public class Reservation {
 	
 	private Person master;
 	private int rooms;
-	private int[] roomNumber;
+	private int[] roomNumbers;
 	
 	public Reservation(Person master, int rooms, int roomNumber[])
 	{
 		this.master = master;
 		this.rooms = rooms;
-		this.roomNumber = roomNumber;
+		this.roomNumbers = roomNumber;
 	}
 
 	public int getRoomNumber(int index)
 	{
-		return roomNumber[index];
+		return roomNumbers[index];
 	}
 	
 	public void setRoomNumber(int index, int value)
 	{
-		this.roomNumber[index] = value;
+		this.roomNumbers[index] = value;
 	}
 	
 	public Person getMaster() {
@@ -39,5 +40,29 @@ public class Reservation {
 
 	public void setRooms(int rooms) {
 		this.rooms = rooms;
+	}
+	
+	public boolean hasRoom(Room room)
+	{
+
+		if(this.rooms != 1)
+		{
+			for(int i = 0; i <this.rooms; i++)
+			{
+				if(this.roomNumbers[i] == room.getNumber())
+				{
+					return true;
+				}
+			}
+		}else
+		{
+			if(this.roomNumbers[0] == room.getNumber())
+			{
+				return true;
+			}
+		}
+		//really didn't need that if statement lol
+		
+		return false;
 	}
 }
