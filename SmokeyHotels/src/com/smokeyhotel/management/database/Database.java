0b54b00a927a56968098e19f76cbf1a03cadd089
InsaneAboutTNT <johnathanminecraftbro@gmail.com>
2016-04-18 22:00:01 +1200

    Set up Database.java

--- SmokeyHotels/src/com/smokeyhotel/management/database/Database.java
+++ SmokeyHotels/src/com/smokeyhotel/management/database/Database.java
@@ -1,5 +1,15 @@
 package com.smokeyhotel.management.database;
 
-public class Database {
+import com.awesome.db.AwesomeDatabase;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+public class Database {
+	AwesomeDatabase awesomeDatabase;
+	public Database() {
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setUser("john");
+		dataSource.setUser("123");
+		dataSource.setURL("jdbc:mysql://122.60.10.170/hotel");
+		awesomeDatabase = new AwesomeDatabase(dataSource);
+	}
 }
