007d699572a0cbb0d1a8e86727bf227d9ae3eddf
SlickJava <slickjava@gmail.com>
2016-04-19 13:51:20 +1200

    Cleaned some command statements up.

--- SmokeyHotels/src/com/smokeyhotel/management/Manager.java
+++ SmokeyHotels/src/com/smokeyhotel/management/Manager.java
@@ -36,21 +36,17 @@ public class Manager {
 	
 	public void initiateCommand(String message)
 	{
-		System.out.println(message + "- Message");
 		String[] split = message.split(" ");		
 		for(Command command : commands)
 		{
 			if(command.getMessage().equals(split[0]))
 			{
-				System.out.println(split[0] + "-" + command.getCommandName());
 				String[] inputs = new String[command.getParameters().length];
 				for(int i = 0; i < command.getParameters().length; i++)
 				{
-					System.out.println("reached");
 					inputs[i] = split[i+1];
 				}
 				command.setInputs(inputs);
-				System.out.println(inputs[0] + "-" + command.getCommandName());
 				command.onExecute();
 				
 			}
