c56c72ea3f3e8409215bfe4a52d9912bd3db7354
InsaneAboutTNT <johnathanminecraftbro@gmail.com>
2016-04-24 18:25:14 +1200

    Edit Room.java

--- SmokeyHotels/src/com/smokeyhotel/room/Room.java
+++ SmokeyHotels/src/com/smokeyhotel/room/Room.java
@@ -13,39 +13,28 @@ public class Room {
 	private boolean vacant;
 	private double price;
 	private Guest[] occupants;
-	private long ID;
 	private int maxNumberOfOccupants;
 	
-	/**
-	 * Constructor:
-	 * Parameter number is the roo
-	 * @param number - the room number
-	 * @param vacant - vacancy of the room
-	 * @param roomState - state of the room, eg. DAMAGED, BEING_CLEANED, MADE
-	 * @param price - price of the room
-	 * @param occupants - array of Guest[] objects occupying the room
-	 */
 
-	public Room(int roomNumber, int maxNumberOfOccupants,boolean vacant,  double price, Guest[] occupants, long ID)
+	/**
+	 * Constructor
+	 * @param roomNumber
+	 * @param vacant
+	 * @param price
+	 * @param occupants
+	 * @param maxNumberOfOccupants
+	 */
+	public Room(int roomNumber, boolean vacant, double price, Guest[] occupants, int maxNumberOfOccupants)
 	{
 		this.number = roomNumber;
 		this.vacant = vacant;
 		this.price = price;
 		this.occupants = occupants;
-		this.ID = ID;
 		this.maxNumberOfOccupants = maxNumberOfOccupants;
 	}
 	
 	public int getMaxNumberOfOccupants() {
 		return maxNumberOfOccupants;
-	}
-
-	public long getID() {
-		return ID;
-	}
-
-	public void setID(long iD) {
-		ID = iD;
 	}
 
 	public int getNumber() {
